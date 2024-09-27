af0000a6bfb0cd670bcb6046216cbbf269338f8c
Fr4n0m <fran11799@outlook.com>
2024-09-27 13:23:18 +0200

    feat(root): Add book pages

---
+++ src/data/books.js
@@ -0,0 +1,50 @@
+export const books = [
+	{
+		id: 'GOT-1',
+		title: 'JUEGO DE TRONOS (CANCIÓN DE HIELO Y FUEGO 1)',
+		image: 'https://imagessl4.casadellibro.com/a/l/s7/34/9788466374934.webp',
+		author: 'George R. R. Martin',
+		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
+		price: 14.2
+	},
+	{
+		id: 'GOT-2',
+		title: 'CHOQUE DE REYES (CANCIÓN DE HIELO Y FUEGO 2)',
+		image: 'https://imagessl1.casadellibro.com/a/l/s7/41/9788466374941.webp',
+		author: 'George R. R. Martin',
+		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
+		price: 14.2
+	},
+	{
+		id: 'GOT-3',
+		title: 'TORMENTA DE ESPADAS (CANCIÓN DE HIELO Y FUEGO 3)',
+		image: 'https://imagessl8.casadellibro.com/a/l/s7/58/9788466374958.webp',
+		author: 'George R. R. Martin',
+		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
+		price: 14.2
+	},
+	{
+		id: 'GOT-4',
+		title: 'FESTÍN DE CUERVOS (CANCIÓN DE HIELO Y FUEGO 4)',
+		image: 'https://imagessl5.casadellibro.com/a/l/s7/65/9788466374965.webp',
+		author: 'George R. R. Martin',
+		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
+		price: 14.2
+	},
+	{
+		id: 'GOT-5',
+		title: 'DANZA DE DRAGONES (CANCIÓN DE HIELO Y FUEGO 5)',
+		image: 'https://imagessl2.casadellibro.com/a/l/s7/72/9788466374972.webp',
+		author: 'George R. R. Martin',
+		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
+		price: 14.2
+	},
+	{
+		id: 'GOT-6',
+		title: 'FUEGO Y SANGRE (CANCIÓN DE HIELO Y FUEGO)',
+		image: 'https://imagessl3.casadellibro.com/a/l/s7/93/9788466356893.webp',
+		author: 'George R. R. Martin',
+		description: '300 AÑOS ANTES DE JUEGO DE TRONOS. (DINASTÍA TARGARYEN: LA CASA DEL DRAGÓN)',
+		price: 14.2
+	}
+]
