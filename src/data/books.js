ba413b6ee95c8bed0d2830c1c622b5c908e1c8c3
Fr4n0m <fran11799@outlook.com>
2024-09-30 17:44:10 +0200

    feat(root): Bookshop finished

--- src/data/books.js
+++ src/data/books.js
@@ -4,6 +4,7 @@ export const books = [
 		title: 'JUEGO DE TRONOS (CANCIÓN DE HIELO Y FUEGO 1)',
 		image: 'https://imagessl4.casadellibro.com/a/l/s7/34/9788466374934.webp',
 		author: 'George R. R. Martin',
+		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
 		price: 14.2
 	},
@@ -12,6 +13,7 @@ export const books = [
 		title: 'CHOQUE DE REYES (CANCIÓN DE HIELO Y FUEGO 2)',
 		image: 'https://imagessl1.casadellibro.com/a/l/s7/41/9788466374941.webp',
 		author: 'George R. R. Martin',
+		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
 		price: 14.2
 	},
@@ -20,6 +22,7 @@ export const books = [
 		title: 'TORMENTA DE ESPADAS (CANCIÓN DE HIELO Y FUEGO 3)',
 		image: 'https://imagessl8.casadellibro.com/a/l/s7/58/9788466374958.webp',
 		author: 'George R. R. Martin',
+		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
 		price: 14.2
 	},
@@ -28,6 +31,7 @@ export const books = [
 		title: 'FESTÍN DE CUERVOS (CANCIÓN DE HIELO Y FUEGO 4)',
 		image: 'https://imagessl5.casadellibro.com/a/l/s7/65/9788466374965.webp',
 		author: 'George R. R. Martin',
+		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
 		price: 14.2
 	},
@@ -36,6 +40,7 @@ export const books = [
 		title: 'DANZA DE DRAGONES (CANCIÓN DE HIELO Y FUEGO 5)',
 		image: 'https://imagessl2.casadellibro.com/a/l/s7/72/9788466374972.webp',
 		author: 'George R. R. Martin',
+		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
 		price: 14.2
 	},
@@ -44,7 +49,35 @@ export const books = [
 		title: 'FUEGO Y SANGRE (CANCIÓN DE HIELO Y FUEGO)',
 		image: 'https://imagessl3.casadellibro.com/a/l/s7/93/9788466356893.webp',
 		author: 'George R. R. Martin',
+		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: '300 AÑOS ANTES DE JUEGO DE TRONOS. (DINASTÍA TARGARYEN: LA CASA DEL DRAGÓN)',
 		price: 14.2
+	},
+	{
+		id: 'El-problema-de-los-tres-cuerpos',
+		title: 'EL PROBLEMA DE LOS TRES CUERPOS',
+		image: 'https://imagessl4.casadellibro.com/a/l/s7/34/9788466659734.webp',
+		author: 'CIXIN LIU',
+		authorLink: 'https://es.wikipedia.org/wiki/Liu_Cixin',
+		description: '(TRILOGIA DE LOS TRES CUERPOS 1) PRIMER VOLUMEN TRILOGÍA',
+		price: 20.8
+	},
+	{
+		id: 'El-bosque-oscuro',
+		title: 'EL BOSQUE OSCURO',
+		image: 'https://imagessl1.casadellibro.com/a/l/s7/21/9788466660921.webp',
+		author: 'CIXIN LIU',
+		authorLink: 'https://es.wikipedia.org/wiki/Liu_Cixin',
+		description: '(TRILOGIA DE LOS TRES CUERPOS 2) SEGUNDO VOLUMEN TRILOGÍA',
+		price: 21.75
+	},
+	{
+		id: 'El-fin-de-la-muerte',
+		title: 'EL FIN DE LA MUERTE',
+		image: 'https://imagessl7.casadellibro.com/a/l/s7/17/9788417347017.webp',
+		author: 'CIXIN LIU',
+		authorLink: 'https://es.wikipedia.org/wiki/Liu_Cixin',
+		description: '(TRILOGÍA DE LOS TRES CUERPOS 3) TERCER VOLUMEN TRILOGÍA',
+		price: 21.75
 	}
 ]
