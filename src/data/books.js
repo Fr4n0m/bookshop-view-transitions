0bc0ec028b526e7d042c153e38b62d3f1891d58c
Francisco Rodriguez <fran11799@outlook.com>
2024-10-01 12:08:30 +0200

    feat(root): Add images

--- src/data/books.js
+++ src/data/books.js
@@ -2,7 +2,7 @@ export const books = [
 	{
 		id: 'GOT-1',
 		title: 'JUEGO DE TRONOS',
-		image: 'https://imagessl4.casadellibro.com/a/l/s7/34/9788466374934.webp',
+		image: '/images/got-1.webp',
 		author: 'George R. R. Martin',
 		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
@@ -11,7 +11,7 @@ export const books = [
 	{
 		id: 'GOT-2',
 		title: 'CHOQUE DE REYES',
-		image: 'https://imagessl1.casadellibro.com/a/l/s7/41/9788466374941.webp',
+		image: '/images/got-2.webp',
 		author: 'George R. R. Martin',
 		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
@@ -20,7 +20,7 @@ export const books = [
 	{
 		id: 'GOT-3',
 		title: 'TORMENTA DE ESPADAS',
-		image: 'https://imagessl8.casadellibro.com/a/l/s7/58/9788466374958.webp',
+		image: '/images/got-3.webp',
 		author: 'George R. R. Martin',
 		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
@@ -29,7 +29,7 @@ export const books = [
 	{
 		id: 'GOT-4',
 		title: 'FESTÍN DE CUERVOS',
-		image: 'https://imagessl5.casadellibro.com/a/l/s7/65/9788466374965.webp',
+		image: '/images/got-4.webp',
 		author: 'George R. R. Martin',
 		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
@@ -38,7 +38,7 @@ export const books = [
 	{
 		id: 'GOT-5',
 		title: 'DANZA DE DRAGONES',
-		image: 'https://imagessl2.casadellibro.com/a/l/s7/72/9788466374972.webp',
+		image: '/images/got-5.webp',
 		author: 'George R. R. Martin',
 		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: 'LOS LIBROS QUE INSPIRARON LA SERIE JUEGO DE TRONOS DE HBO',
@@ -47,7 +47,7 @@ export const books = [
 	{
 		id: 'GOT-6',
 		title: 'FUEGO Y SANGRE',
-		image: 'https://imagessl3.casadellibro.com/a/l/s7/93/9788466356893.webp',
+		image: '/images/fuego-y-sangre.webp',
 		author: 'George R. R. Martin',
 		authorLink: 'https://es.wikipedia.org/wiki/George_R._R._Martin',
 		description: '300 AÑOS ANTES DE JUEGO DE TRONOS. (DINASTÍA TARGARYEN: LA CASA DEL DRAGÓN)',
@@ -56,7 +56,7 @@ export const books = [
 	{
 		id: 'El-problema-de-los-tres-cuerpos',
 		title: 'EL PROBLEMA DE LOS TRES CUERPOS',
-		image: 'https://imagessl4.casadellibro.com/a/l/s7/34/9788466659734.webp',
+		image: '/images/el-problema-de-los-tres-cuerpos-1.webp',
 		author: 'CIXIN LIU',
 		authorLink: 'https://es.wikipedia.org/wiki/Liu_Cixin',
 		description: '(TRILOGIA DE LOS TRES CUERPOS 1) PRIMER VOLUMEN TRILOGÍA',
@@ -65,7 +65,7 @@ export const books = [
 	{
 		id: 'El-bosque-oscuro',
 		title: 'EL BOSQUE OSCURO',
-		image: 'https://imagessl1.casadellibro.com/a/l/s7/21/9788466660921.webp',
+		image: '/images/el-problema-de-los-tres-cuerpos-2.webp',
 		author: 'CIXIN LIU',
 		authorLink: 'https://es.wikipedia.org/wiki/Liu_Cixin',
 		description: '(TRILOGIA DE LOS TRES CUERPOS 2) SEGUNDO VOLUMEN TRILOGÍA',
@@ -74,7 +74,7 @@ export const books = [
 	{
 		id: 'El-fin-de-la-muerte',
 		title: 'EL FIN DE LA MUERTE',
-		image: 'https://imagessl7.casadellibro.com/a/l/s7/17/9788417347017.webp',
+		image: '/images/el-problema-de-los-tres-cuerpos-3.webp',
 		author: 'CIXIN LIU',
 		authorLink: 'https://es.wikipedia.org/wiki/Liu_Cixin',
 		description: '(TRILOGÍA DE LOS TRES CUERPOS 3) TERCER VOLUMEN TRILOGÍA',
